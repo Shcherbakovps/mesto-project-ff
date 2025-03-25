978daa2ea578ddd484021206f28aad48f6c9e863
Shcherbakov_PS <p.s.shcherbakov@gmail.com>
2025-03-25 13:21:45 +0400

    First commit: createCard and removeCard functions has been added

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
-
+git@github.com:Shcherbakovps/mesto-project-ff.git
+Ссылка: https://github.com/Shcherbakovps/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(item, removeCard) {
+    const newCard = cardTemplate.cloneNode(true);
 
+    newCard.querySelector('.card__image').src = item.link;
+    newCard.querySelector('.card__title').textContent = item.name;
+    newCard.querySelector('.card__image').alt = item.name;
+
+    newCard.querySelector('.card__delete-button').addEventListener('click', removeCard);
+    
+    placesList.append(newCard);
+}  
 // @todo: Функция удаления карточки
-
+function removeCard(item) {
+    const cardToRemove = item.target.closest('.card');
+    cardToRemove.remove(item);
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (item) {
+    createCard(item, removeCard);
+});
